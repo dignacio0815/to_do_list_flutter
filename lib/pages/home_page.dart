c522021e63033607fdaf46686f1b992b7adfda06
Denize Ignacio <denizeignacio@gmail.com>
2023-02-11 22:16:35 -0800

    User can now delete todo tasks in their list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -51,6 +51,13 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // delete task
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,6 +77,7 @@ class _HomePageState extends State<HomePage> {
             taskName: toDoList[index][0],
             taskCompleted: toDoList[index][1],
             onChanged: (value) => checkBoxChanged(value, index),
+            deleteFunction: (context) => deleteTask(index),
           );
         },
       ),
