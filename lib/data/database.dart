8894fb0e71cce76c948c07298a52ddb2be2f6a19
Denize Ignacio <denizeignacio@gmail.com>
2023-02-11 22:30:02 -0800

    Integrate hive db for data persistence

---
+++ lib/data/database.dart
@@ -0,0 +1,26 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDataBase {
+  List toDoList = [];
+
+  // reference our box
+  final _myBox = Hive.box("myBox");
+
+  // run this method if this is the first time ever opening the app
+  void createInitialData() {
+    toDoList = [
+      ["Make Tutorial", false],
+      ["Do exercise", false],
+    ];
+  }
+
+  // load the data from database
+  void loadData() {
+    toDoList = _myBox.get("TODOLIST");
+  }
+
+  // update the database
+  void updateDatabase() {
+    _myBox.put("TODOLIST", toDoList);
+  }
+}
